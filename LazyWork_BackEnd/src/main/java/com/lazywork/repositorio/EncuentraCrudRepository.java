e71e60e681d1645d5745b02ed750d5c37a82e05d
dove <jdtorres5834@gmail.com>
2023-08-28 13:50:34 -0500

    Actualizacion back, agregar foreign keys, etc

--- LazyWork_BackEnd/src/main/java/com/lazywork/repositorio/EncuentraCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.Encuentra;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EncuentraCrudRepository  extends CrudRepository<Encuentra, String> {
-}
