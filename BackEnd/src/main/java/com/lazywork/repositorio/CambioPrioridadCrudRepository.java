a8bf99f9e8869047dce425da30dbe1874e43f2c5
JulianGuz <juliandaguzz@gmail.com>
2023-09-09 19:34:43 -0500

    ...

--- BackEnd/src/main/java/com/lazywork/repositorio/CambioPrioridadCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.CambioPrioridad;
-import com.lazywork.entidad.Rol;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CambioPrioridadCrudRepository extends CrudRepository<CambioPrioridad, String> {
-}
-
--- BackEnd/src/main/java/com/lazywork/repositorio/IncidenciaCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.Incidencia;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IncidenciaCrudRepository extends CrudRepository<Incidencia, String> {
-}
