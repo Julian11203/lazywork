e4f06c1b3b96db814dacda1ee4215ce5ebea3a3e
JulianGuz <juliandaguzz@gmail.com>
2023-09-11 19:00:22 -0500

    update RolCrudRepository

--- BackEnd/src/main/java/com/lazywork/repositorio/RolCrudRepository.java
+++ BackEnd/src/main/java/com/lazywork/repositorio/RolCrudRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface RolCrudRepository extends CrudRepository<Rol, String> {
-    @Query("SELECT CASE WHEN COUNT(ur) > 0 THEN true ELSE false END FROM UsuarioRol ur INNER JOIN " +
-            "ur.rol rol WHERE rol.id = :rolId")
+    @Query("SELECT CASE WHEN COUNT(ur) > 0 THEN true ELSE false END FROM UsuarioRol ur INNER JOIN ur.rol rol WHERE rol.id = :rolId")
     boolean existsInUsuarioRol(@Param("rolId") String rolId);
 }
