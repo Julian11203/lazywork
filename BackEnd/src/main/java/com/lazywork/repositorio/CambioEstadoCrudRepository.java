259686e385556ff3d3f7736bf0e2dbc83a6b192b
edilsonbedoya <137055777+edilsonbedoya@users.noreply.github.com>
2023-10-07 18:43:23 -0500

    Delete BackEnd directory

--- BackEnd/src/main/java/com/lazywork/repositorio/CambioEstadoCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.CambioEstado;
-import com.lazywork.entidad.CambioPrioridad;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CambioEstadoCrudRepository extends CrudRepository<CambioEstado, Long> {
-}
-
