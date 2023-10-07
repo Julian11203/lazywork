491d0b343ec6e47a982a4edcd5d50fca5c041f72
Jdtorresa <118572427+Jdtorresa@users.noreply.github.com>
2023-10-07 11:31:50 -0500

    Delete BackEnd directory

--- BackEnd/src/main/java/com/lazywork/repositorio/PrioridadesIncidenciasCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.PrioridadIncidencia;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PrioridadesIncidenciasCrudRepository extends CrudRepository<PrioridadIncidencia, Long> {
-}
--- BackEnd/src/main/java/com/lazywork/repositorio/UsuarioCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioCrudRepository extends CrudRepository<Usuario, Long> {
-}
