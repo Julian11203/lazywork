d2f8f0a497c8c3ce20995ab219b3ba5e203d0bdd
edilsonbedoya <137055777+edilsonbedoya@users.noreply.github.com>
2023-10-07 18:48:03 -0500

    Cambios

---
+++ BackEnd/src/main/java/com/lazywork/LazyworkApplication.java
@@ -0,0 +1,13 @@
+package com.lazywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyworkApplication.class, args);
+	}
+
+}
