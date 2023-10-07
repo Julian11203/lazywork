6b2c68ecf63c3e48fbe7bf47509064e81369a457
edilsonbedoya <137055777+edilsonbedoya@users.noreply.github.com>
2023-10-07 18:33:17 -0500

    Delete BackEnd directory

--- BackEnd/src/main/java/com/lazywork/repositorio/EstadosIncidenciasCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lazywork.repositorio;
-
-import com.lazywork.entidad.EstadoIncidencia;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EstadosIncidenciasCrudRepository extends CrudRepository<EstadoIncidencia, Long> {
-}
