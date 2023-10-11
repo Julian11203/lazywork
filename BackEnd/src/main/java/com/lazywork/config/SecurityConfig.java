2c8399e63be8329837024d5b22dd64504801f3bc
JOSE DANIEL <juliandaguzz@gmail.com>
2023-10-11 18:34:57 -0500

    update config authentication

--- BackEnd/src/main/java/com/lazywork/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/lazywork/config/SecurityConfig.java
@@ -5,10 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+    @Bean
+    public UserDetailsService geters(){
+        var user  = User.withUsername("postgres")
+                .password("lazy250217300")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
