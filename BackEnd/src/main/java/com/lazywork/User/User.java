5682f355a80aa42ad5d4668a5372a4d9a4628a33
JulianGuz <juliandaguzz@gmail.com>
2023-10-29 20:42:09 -0500

    Login and request finalizy :)

--- BackEnd/src/main/java/com/lazywork/User/User.java
+++ BackEnd/src/main/java/com/lazywork/User/User.java
@@ -41,12 +41,12 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return contraseña;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
