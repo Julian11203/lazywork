df6fc2c9c97062e4e6ec808e74df661f312c921d
JulianDAGUZ <juliandaguzz@gmail.com>
2024-02-21 19:47:59 -0500

    Base funcional

--- Back&Front/src/main/resources/static/funciones.js
+++ Back&Front/src/main/resources/static/funciones.js
@@ -1,10 +1,10 @@
 function mostrarInfoAlUsuario() {
     $.get("/user", function(data){
-        var gestionVisitante = document.querySelector("#GestionVisitante");
         var gestionUsuario = document.querySelector("#GestionUser");
         var gestionAdministrador = document.querySelector("#GestionAdmin");
         var iniciarSesionBtn = document.querySelector("#iniciarSesionBtn");
         var cerrarSesionBtn = document.querySelector("#cerrarSesionBtn");
+        var nombreCompleto = document.querySelector("#nombreCompleto");
 
         if(data!=null){
             $("#nombreCompleto").html(data.nombreCompleto);
@@ -12,21 +12,17 @@ function mostrarInfoAlUsuario() {
             $("#rolDeUsuario").html(data.rolDeUsuario);
             if (data.rolDeUsuario == 'USER') {
                 gestionAdministrador.style.display = 'none';
-                gestionVisitante.style.display = 'none';
                 iniciarSesionBtn.style.display = 'none';
             }
             else if (data.rolDeUsuario == 'ADMIN') {
                 gestionUsuario.style.display = 'none';
-                gestionVisitante.style.display = 'none';
                 iniciarSesionBtn.style.display = 'none';
             }
             else{
                 cerrarSesionBtn.style.display = 'none';
+                gestionAdministrador.style.display = 'none';
+                gestionUsuario.style.display = 'none';
             }
-        }
-        else{
-            gestionAdministrador.style.display = 'none';
-            gestionUsuario.style.display = 'none';
         }
     });
 }
