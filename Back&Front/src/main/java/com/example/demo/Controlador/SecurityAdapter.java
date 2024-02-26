fdddf40d1f684d0b7df8554aa6524968971eecaf
Julian David Guzman Caballero <jguzmanc@agrosavia.co>
2024-02-26 15:24:09 -0500

    Base Funcional

--- Back&Front/src/main/java/com/example/demo/Controlador/SecurityAdapter.java
+++ Back&Front/src/main/java/com/example/demo/Controlador/SecurityAdapter.java
@@ -1,9 +1,11 @@
 package com.example.demo.Controlador;
 
 
+import antlr.Token;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,24 +16,20 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .authorizeHttpRequests(a -> {
-                    try {
-                        a
-                                .antMatchers("/", "/**/funciones.js", "/**/code.jquery.com_jquery-3.7.0.min.js").permitAll()
-                                .anyRequest().authenticated()
-                                .and()
-                                .oauth2Login()
-                                .and()
-                                .logout()
-                                .logoutUrl("/logout")
-                                .logoutSuccessUrl("http://localhost:8080/").permitAll();
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .exceptionHandling(e -> e
+                .logout()
+                .logoutUrl("/logout") // Especifica la URL de logout
+                .logoutSuccessUrl("http://localhost:8080/") // Establece la URL a la que se redireccionará después del logout
+                .permitAll() // Permite a cualquier usuario acceder a la URL de logout
+
+                .and()
+
+                .authorizeHttpRequests(a -> a
+                        .antMatchers("/**", "/**/funciones.js", "/**/code.jquery.com_jquery-3.7.0.min.js").permitAll()
+                        .anyRequest().authenticated()
+                ).exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                );
+                )
+                .oauth2Login();
 
         http.cors().and().csrf().disable();
     }
